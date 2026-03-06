96941bdab5a2659e2601732f2c525f1a3ac2b642
Daniel Iván Morales Baca <ddimb6002@gmail.com>
2026-03-06 16:36:40 -0700

    Se añadio lo que faltaba de implementar en la logica de burbuja.

--- src/main/java/mx/itson/aa_uc2_269990/Ordenamientos.java
+++ src/main/java/mx/itson/aa_uc2_269990/Ordenamientos.java
@@ -8,27 +8,33 @@ package mx.itson.aa_uc2_269990;
  *
  * @author Daniel
  */
-public class Ordenamientos {
+public class Ordenamientos {//complejidad temp // conteo
     
-    public static void burbuja(int[] arreglo) {
+    public static void burbuja(int[] arreglo) { // arreglo inicial: [10, 4, 24, 3, 8, 12]
+                        
+        int n = arreglo.length; // 1 // n = 6
 
-        int n = arreglo.length; // 1
+            for (int i = 0; i < n - 1; i++) { // 1 + n + n-1 // i=0:[10,4,24,3,8,12] i=1:[4,10,3,8,12,24]
 
-            for (int i = 0; i < n - 1; i++) { // 1 + n + n-1
-
-                for (int j = 0; j < n - 1; j++) { // 1 + n + n-1
+                for (int j = 0; j < n - 1; j++) { // 1 + n + n-1 // j=0:10>4 correcto j=1:10>24 incorrecto j=2:...
 
                     // Comparación de elementos
-                    if (arreglo[j] > arreglo[j + 1]) { // 1
+                    if (arreglo[j] > arreglo[j + 1]) { // 1 // compara par actual con siguiente
                         // Intercambio de valores
-                        int aux = arreglo[j];        // 1 
-                        arreglo[j] = arreglo[j + 1];  // 1
-                        arreglo[j + 1] = aux;        // 1 
+                        int aux = arreglo[j];        // 1  // guarda elemento mayor: aux=10
+                        arreglo[j] = arreglo[j + 1];  // 1 // coloca el menor en posición actual
+                        arreglo[j + 1] = aux;        // 1 // coloca aux en posición siguiente
                     }
                 }
             }
-               //1 + (2n) + (n-1) * [2n + (n-1) * 4]
-               //Complejidad temporal: O(n^2)
+                //1 + (2n) + (n-1) * [2n + (n-1) * 4]
+                //Complejidad temporal: O(n^2)
+                //Arreglo inicial:    [10,  4, 24,  3,  8, 12]
+                //Tras i=0:           [ 4, 10,  3,  8, 12, 24]
+                //Tras i=1:           [ 4,  3,  8, 10, 12, 24]
+                //Tras i=2:           [ 3,  4,  8, 10, 12, 24]
+                //Tras i=3,4:         [ 3,  4,  8, 10, 12, 24] (sin cambios)
+                //Arreglo final:      [ 3,  4,  8, 10, 12, 24]
     }
 
     public static void seleccionSort(int[] arreglo) {
